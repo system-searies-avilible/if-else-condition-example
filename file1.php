8171170dfa0b5f60c362eb1d6e59fa032e2750c7
system-searies-avilible <111744571+system-searies-avilible@users.noreply.github.com>
2022-08-30 08:36:37 +0400

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,15 @@
+<?php
+
+$name="moheez";
+$age="23";
+
+if($name=="moheez"){
+	if($age=="23"){
+		echo"You are moheez and you are 23 year old";
+		
+	}	
+}
+else{
+	echo"you are not moheeza";
+}
+?>
